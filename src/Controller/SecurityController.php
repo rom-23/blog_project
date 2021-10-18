1aa87916fd66a66ed60a9bc882bcb4a174b34952
romain-webgroup <romain.laurent23@gmail.com>
2021-10-18 02:51:44 +0200

    authentication+forgot password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -61,6 +61,6 @@ class SecurityController extends AbstractController
      */
     public function denied(): Response
     {
-        return $this->render('security/access_denied.html.twig');
+        return $this->render('security/access-denied.html.twig');
     }
 }
